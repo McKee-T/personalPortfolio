2357d4549436ab80fb60a1f6ce0ce340413f7f2e
McKeeT <Tmckee3232@gmail.com>
2024-02-22 20:18:40 -0500

    initial code done, fine tweaks needed

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll('.nav-links a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetSection = document.querySelector(this.getAttribute('href'));
+            targetSection.scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+});
